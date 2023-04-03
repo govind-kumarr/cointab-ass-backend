8703df89ec95fefb0510de0bcc4edc317b1df5a7
Govind Kumar <gk4051668@gmail.com>
2023-04-03 14:22:16 +0530

    refactored

--- app.js
+++ app.js
@@ -4,16 +4,19 @@ import { makeConnection } from "./config/db.js";
 import { UserRouter } from "./routes/user.routes.js";
 
 config();
-const port = process.env.PORT || 8080;
 const app = express();
 
 app.use(express.json());
 
-app.use("/", (req, res) => res.status(200).send("App is running at", port));
+app.use("/", (req, res) =>
+  res.status(200).send("App is running at", process.env.PORT)
+);
 
 app.use("/user", UserRouter);
 
 const startApp = () =>
-  app.listen(port, () => console.log("listening on port " + port));
+  app.listen(process.env.PORT, () =>
+    console.log("listening on port " + process.env.PORT)
+  );
 
 makeConnection(startApp);
