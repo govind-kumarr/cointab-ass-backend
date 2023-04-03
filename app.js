56d20dbc85b4f88265ee73bd745255ad193fc0a3
Govind Kumar <gk4051668@gmail.com>
2023-04-03 14:12:44 +0530

    refactored

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const app = express();
 
 app.use(express.json());
 
-app.use("/", "App is running at", port);
+app.use("/", (req, res) => res.send("App is running at", port));
 
 app.use("/user", UserRouter);
 
--- cointab-ass-frontend/src/pages/Signup.jsx
+++ cointab-ass-frontend/src/pages/Signup.jsx
@@ -1,22 +1,30 @@
 import React from "react";
 import { Box, Button, Modal, TextField, Typography } from "@mui/material";
+import { baseUrl } from "../App";
 
 export const Signup = () => {
-
   const makeSignUp = async (data) => {
-    try{
-      const response = await fetch(`${}`)
+    try {
+      const response = await fetch(`${baseUrl}user/signup`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(data),
+      });
+      const responseData = await response.json();
+      console.log(responseData);
+    } catch (error) {
+      console.log("Error while signing up\n", error);
     }
-  }
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
     let form = e.target;
-
     let data = new FormData(form);
     data = Object.fromEntries(data);
 
     console.log(data);
+    makeSignUp(data);
     form.reset();
   };
   return (
